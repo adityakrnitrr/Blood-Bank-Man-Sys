ad1b82a97515b94207bb1ce278d111d87bf142ca
Aditya Kumar <aditya7589@gmail.com>
2022-08-24 20:37:10 +0530

    all files added

---
+++ src/main/java/Project/ConnectionProvider.java
@@ -0,0 +1,20 @@
+package Project;
+import java.sql.*;
+import java.sql.Connection;
+public class ConnectionProvider {
+
+	public static Connection getCon()
+	{
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bloodbank","root","Aadi@123");
+			return con;
+		}
+		catch(Exception e)
+		{
+			System.out.print(e);
+			return null;
+		}
+	}
+}
